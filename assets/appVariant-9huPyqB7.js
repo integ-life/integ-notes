785d1bc6ea4d09f4585d68f269f02d532271bf57
Albert Song <flyfy1@users.noreply.github.com>
2026-08-21 08:20:25 +0800

    Deploy Notes GitHub Pages

---
+++ assets/appVariant-9huPyqB7.js
@@ -0,0 +1,1 @@
+const t="https://money.integ.life",e="https://todo.integ.life",s="https://people.integ.life",a="https://calendar.integ.life",i="";export{t as a,a as c,i as o,s as p,e as t};
---
+++ assets/useAIAgents-CbVDfhpU.js
@@ -0,0 +1,1 @@
+import{r as e,u as t}from"./vendor-BNNCcV2J.js";import{F as s}from"./index-IDG9bEVz.js";const r=()=>{const[r,n]=e.useState([]),[a,o]=e.useState(null),u=t(e=>e.isOfflineMode),f=u?[]:r;return e.useEffect(()=>{if(u)return;let e=!0;return s.listAIAgents().then(t=>{e&&n(t.agents||[])}).catch(t=>{e&&o(t.message)}),()=>{e=!1}},[u]),{agents:f,error:a}};export{r as u};
